bb81430906cab21054fbba36bd89671b7c92e8dc
Minh Dang Cong <congminh.codecomplete@gmail.com>
2018-12-10 10:20:55 +0700

    duc cum

--- app/src/main/java/com/example/tam/cnpm/service/retrofit2/APIUtils.java
+++ app/src/main/java/com/example/tam/cnpm/service/retrofit2/APIUtils.java
@@ -1,8 +1,8 @@
 package com.example.tam.cnpm.service.retrofit2;
 public class APIUtils {
     //https://congminhdang14t3vn.000webhostapp.com/appbanhang/
-    //public static final String baseUrl = "https://congminhdang14t3vn.000webhostapp.com/appbanhang/";
-    public static final String baseUrl = "https://hrm.dev.codecomplete.asia/api/";
+    public static final String baseUrl = "http://52.14.71.211/api/";
+    //public static final String baseUrl = "https://hrm.dev.codecomplete.asia/api/";
 
     private static DataClient sInstance;
 
